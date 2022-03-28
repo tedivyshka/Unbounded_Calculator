3e8b312c963cb85c1d6c357734395a8b55c2ce14
nafyssat <nafyssatamohamedhalim@gmail.com>
2022-03-28 13:38:14 +0200

    unbouded_int.h

--- unbounded_int.h
+++ unbounded_int.h
@@ -4,11 +4,19 @@ tydef struct{
   chiffre *premier;
   chiffre *dernier;
 }unbounded_int;
+//
 unbounded_int string2unbounded(const char *e);
+//
 unbounded_int ll2unbouded_int(long long i);
+//
 char *unbounded_int2string(unbounded_int i);
+//
 int unbounded_int_cmp_unbounded_int(unbounded_int a,unbounded_int b);
+//
 int unbounded_int_cmp_ll(unbounded_inta,long long b);
+//
 unbounded_int unbounded_int_somme(unbounded_inta,unbounded_int b);
+//
 unbounded_int unbounded_int_difference(unbounded_inta,unbounded_int b);
+//
 unbounded_int unbounded_int_produit(unbounded_inta,unbounded_int b);
