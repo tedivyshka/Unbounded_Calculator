9b23a509160f2f4d571e0a361d3eefe8a9134936
Tedi Vyshka <tedi.vyshka@gmail.com>
2022-03-28 16:30:53 +0200

    Added string2unbounded_int ll2unbounded_int unbounded_int2string

---
+++ unbounded_int.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "unbounded_int.h"
+
+
+unbounded_int string2unbounded_int(const char *e){
+  unbounded_int res;
+  int count = 0;
+  if(e[0]=='+' || e[0]=='-'){
+    res.signe=e[0];
+    count++;
+  }
+  else{
+    res.signe=(e[0] >= '0' && e[0] <= '9')?'+':'*';
+  }
+
+  chiffre* current = malloc(sizeof(chiffre));
+  current->c = e[count];
+  if(!(e[count] >= '0' && e[count] <= '9'))
+    res.signe = '*';
+  res.premier = current;
+  count += 1;
+
+  while(e[count] != '\0'){
+    chiffre* next = malloc(sizeof(chiffre));
+    next->c = e[count];
+    if(!(e[count] >= '0' && e[count] <= '9'))
+      res.signe = '*';
+    current->suivant = next;
+    next->precedent = current;
+    current = next;
+    count+=1;
+  }
+  res.dernier = current;
+  res.len = count;
+  return res;
+}
+
+unbounded_int ll2unbounded_int(long long i) {
+    unbounded_int res;
+    res.signe = (i<0)?'-':'+';
+    i = (i<0)?-i:i;
+    int count = 0;
+
+    chiffre* current = malloc(sizeof(chiffre));
+    current->c = (i%10) + '0';
+    res.dernier = current;
+    i = i/10;
+    count += 1;
+
+    while(i != 0){
+      chiffre* precedent = malloc(sizeof(chiffre));
+      precedent->c = (i%10) + '0';
+      current->precedent = precedent;
+      precedent->suivant = current;
+      current = precedent;
+      i = i/10;
+      count += 1;
+    }
+    res.premier = current;
+    res.len = count;
+    return res;
+}
+
+char *unbounded_int2string(unbounded_int i){
+  char* res = malloc(sizeof(char)*i.len);
+  *res = i.signe;
+  chiffre* current = i.premier;
+  int count = sizeof(char);
+  while(current != NULL){
+    *(res + count) = current->c;
+    count+=sizeof(char);
+    current = current->suivant;
+  }
+  return res;
+}
+
+void test(long long i){
+  i = (i<0)?-i:i;
+  while(i != 0){
+    printf("%c %lld \n",i%10+'0',i);
+    i = i/10;
+  }
+}
+
+int main(){
+
+  unbounded_int a = string2unbounded("234z");
+  char* char_a = unbounded_int2string(a);
+  printf("%s\n",char_a);
+
+  unbounded_int b = ll2unbounded_int(-239424923948);
+  char* char_b = unbounded_int2string(b);
+  printf("%s\n",char_b);
+
+  return 0;
+}
--- unbounded_int.h
+++ unbounded_int.h
@@ -1,11 +1,18 @@
-tydef struct{
+typedef struct chiffre{
+  struct chiffre *suivant;
+  char c;
+  struct chiffre *precedent;
+} chiffre;
+
+typedef struct{
   char signe;
   size_t len ;
   chiffre *premier;
   chiffre *dernier;
-}unbounded_int;
+} unbounded_int;
+
 //
-unbounded_int string2unbounded(const char *e);
+unbounded_int string2unbounded_int(const char *e);
 //
 unbounded_int ll2unbouded_int(long long i);
 //
@@ -13,10 +20,10 @@ char *unbounded_int2string(unbounded_int i);
 //
 int unbounded_int_cmp_unbounded_int(unbounded_int a,unbounded_int b);
 //
-int unbounded_int_cmp_ll(unbounded_inta,long long b);
+int unbounded_int_cmp_ll(unbounded_int a,long long b);
 //
-unbounded_int unbounded_int_somme(unbounded_inta,unbounded_int b);
+unbounded_int unbounded_int_somme(unbounded_int a,unbounded_int b);
 //
-unbounded_int unbounded_int_difference(unbounded_inta,unbounded_int b);
+unbounded_int unbounded_int_difference(unbounded_int a,unbounded_int b);
 //
-unbounded_int unbounded_int_produit(unbounded_inta,unbounded_int b);
+unbounded_int unbounded_int_produit(unbounded_int a,unbounded_int b);
