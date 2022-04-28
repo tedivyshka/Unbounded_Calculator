65590c3a545dcdb12eb1cfdc8c8ca02945321b29
nafyssat <nafyssatamohamedhalim@gmail.com>
2022-04-28 13:23:13 +0200

    exo 2

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include "unbounded_int.h"
+
+
+int main(int argc, char const *argv[])
+{
+    FILE * tmp=fopen("src","r"); 
+    char ligne[100];
+    char var[50];
+    char dig[50];
+    int i=0;
+    
+    while(!feof(tmp)){
+        char c=fgetc(tmp);
+        if(c!=' ' && c!='\n'){
+            ligne[i]=c;
+            i++;
+            
+        }
+        if(c=='\n'){
+            int k=0;
+            int z=0;
+                for(int i=0;i<strlen(ligne);i++){
+                    
+                    if(ligne[i]=='='){
+                       continue;
+                    }
+                    if(ligne[i]<='z' || ligne[i]>='a'){
+                       var[z]=ligne[i];
+                       z++;
+                    }
+                   if(isdigit(ligne[i])){
+                       dig[k]=ligne[i];
+                       k++;
+                    }
+                    
+                }
+            }
+        
+       
+        
+    }
+
+}
+
+
 
